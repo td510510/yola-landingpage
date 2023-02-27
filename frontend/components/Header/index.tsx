c94864f28f1037dae99316f2fd8c8352fcc6a5c6
td510510 <89639667+td510510@users.noreply.github.com>
2023-02-27 17:23:40 +0700

    Feature/menu mobile (#3)
    * header
    
    * count
    
    * menu mobile
    
    ---------
    
    Co-authored-by: thinh.dang <thinh.dang@skyblazestudios.com>

--- frontend/components/Header/index.tsx
+++ frontend/components/Header/index.tsx
@@ -1,9 +1,24 @@
 import Image from 'next/image'
+import { useState, useRef } from 'react'
+import useOnClickOutside from '@/hooks/useOnClickOutside'
 import Button from '../Button'
 import styles from './Header.module.scss'
 import HamBurger from '../../public/svg/menu.svg'
+import CloseIcon from '../../public/svg/close-menu.svg'
 
 const Header = () => {
+  const [isShowMobileMenu, setIsShowMobileMenu] = useState(false)
+  const mobileMenuRef = useRef(null)
+  useOnClickOutside(mobileMenuRef, () => setIsShowMobileMenu(false))
+
+  const showMobileMenu = () => {
+    setIsShowMobileMenu(true)
+  }
+
+  const closeMobileMenu = () => {
+    setIsShowMobileMenu(false)
+  }
+
   return (
     <header className={styles.container}>
       <div className={styles.wrapper}>
@@ -21,10 +36,21 @@ const Header = () => {
         <div className={styles.right}>
           <div className={styles.signIn}>Sign In</div>
           <Button classStyles={styles.signUp} text='Sign Up' />
-          <div className={styles.hamburger}>
+          <div className={styles.hamburger} onClick={showMobileMenu}>
             <HamBurger />
           </div>
         </div>
+      </div>
+      <div className={`${styles.mobileMenu} ${isShowMobileMenu ? styles.show : styles.hide}`} ref={mobileMenuRef}>
+        <div className={styles.closeIcon} onClick={closeMobileMenu}>
+          <CloseIcon />
+        </div>
+        <ul role='list'>
+          <li>Home</li>
+          <li>Classes</li>
+          <li>Features</li>
+          <li>Contact</li>
+        </ul>
       </div>
     </header>
   )
